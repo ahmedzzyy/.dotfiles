1aaf229c41429f19ce8c98d11af9b76f6177e509
Ahmed S. <s.ahmed.profiquence@gmail.com>
2026-02-24 19:34:29 +0530

    Update nvim config

--- nvim/.config/nvim/lua/ahmedzzyy/plugins/linting.lua
+++ nvim/.config/nvim/lua/ahmedzzyy/plugins/linting.lua
@@ -15,6 +15,12 @@ return {
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+		local function try_linting()
+			local linters = lint.linters_by_ft[vim.bo.filetype]
+
+			lint.try_lint(linters)
+		end
+
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
--- nvim/.config/nvim/lua/ahmedzzyy/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/ahmedzzyy/plugins/neo-tree.lua
@@ -1,28 +1,28 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    enable_git_status = true,
-    enable_diagnostics = true,
-    filesystem = {
-        filtered_items = {
-            hide_gitignored = false,  -- show git‑ignored files
-        },
-    },
-    config = function()
-        vim.keymap.set(
-            "n", "<leader>e", "<Cmd>Neotree<CR>",
-            { desc = "Focus File Explorer" }
-        )
-        vim.keymap.set(
-            "n", "<leader>et", "<Cmd>Neotree toggle reveal<CR>",
-            { desc = "Toggle File Explorer" }
-        )
-        -- TODO add keymaps for collapse and maybe refresh later
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons", -- optional, but recommended
+	},
+	lazy = false, -- neo-tree will lazily load itself
+
+	config = function()
+		require("neo-tree").setup({
+			enable_git_status = true,
+			enable_diagnostics = true,
+			filesystem = {
+				filtered_items = {
+					hide_gitignored = false,
+					hide_dotfiles = false,
+					hide_ignored = false,
+					hide_hidden = false,
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>", { desc = "Focus File Explorer" })
+		vim.keymap.set("n", "<leader>et", "<Cmd>Neotree toggle reveal<CR>", { desc = "Toggle File Explorer" })
+		-- TODO add keymaps for collapse and maybe refresh later
+	end,
 }
